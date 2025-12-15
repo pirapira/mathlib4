b07b008731e6944f8d61f8a0c3f4c8fb62ba7b2c
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-15 20:35:03 +0100

    Simplify

--- Mathlib/Algebra/Ring/GrindInstances.lean
+++ Mathlib/Algebra/Ring/GrindInstances.lean
@@ -68,38 +68,3 @@ instance (priority := 100) Ring.toGrindRing [s : Ring α] :
     | _ + 2 => Int.cast_ofNat _
     intCast_neg := Int.cast_neg
     zsmul_natCast_eq_nsmul n a := natCast_zsmul a n }
-
-instance (priority := 100) CommRing.toGrindCommRing [s : CommRing α] :
-    Grind.CommRing α :=
-  { Ring.toGrindRing α with
-    mul_comm := s.mul_comm }
-
-theorem Semiring.toGrindSemiring_ofNat [Semiring α] (n : ℕ) :
-    @OfNat.ofNat α n (Lean.Grind.Semiring.ofNat n) = n.cast := by
-  match n with
-  | 0 => simp
-  | 1 => simp
-  | n + 2 => rfl
-
-attribute [local instance] Grind.Semiring.natCast Grind.Ring.intCast in
--- Verify that we can construct a `CommRing` from a `Lean.Grind.CommRing`.
--- This is not an instance (or even a `def`) because this direction should never be used.
--- There is no reason to expect that using `CommRing.toGrindCommRing` and then this construction
--- will give a result defeq to the original `CommRing α`.
-example (s : Grind.CommRing α) : CommRing α :=
-  { s with
-    zero_add := Grind.AddCommMonoid.zero_add
-    right_distrib := Grind.Semiring.right_distrib
-    mul_zero := Grind.Semiring.mul_zero
-    one_mul := Grind.Semiring.one_mul
-    nsmul := nsmulRec
-    zsmul := zsmulRec
-    npow := npowRec
-    natCast := Nat.cast
-    natCast_zero :=  Grind.Semiring.natCast_zero
-    natCast_succ n := Grind.Semiring.natCast_succ n
-    intCast := Int.cast
-    intCast_ofNat := Grind.Ring.intCast_natCast
-    intCast_negSucc n := by
-      rw [Int.negSucc_eq, Grind.Ring.intCast_neg,
-        Grind.Ring.intCast_natCast_add_one, Grind.Semiring.natCast_succ] }
