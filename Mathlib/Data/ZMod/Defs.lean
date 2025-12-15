4b18cbb7a4eeb0fd4ae2ed43045847c32d4512dc
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-15 20:42:37 +0100

    simplify

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -5,7 +5,47 @@ Authors: Eric Rodriguez
 -/
 module
 
-public import Mathlib.Algebra.Ring.GrindInstances
+public import Mathlib.Algebra.Ring.Defs
+public import Mathlib.Data.Int.Cast.Basic
+
+/-!
+# Instances for `grind`.
+-/
+
+@[expose] public section
+
+open Lean
+
+variable (α : Type*)
+
+-- This is a low priority instance so that the built-in `Lean.Grind.Semiring Nat` instance
+-- (which has a non-defeq `ofNat` instance) is used preferentially.
+instance (priority := 100) Semiring.toGrindSemiring [s : Semiring α] :
+    Grind.Semiring α :=
+  { s with
+    nsmul := ⟨s.nsmul⟩
+    npow := ⟨fun a n => a^n⟩
+    ofNat | 0 | 1 | n + 2 => inferInstance
+    natCast := inferInstance
+    add_zero := by simp [add_zero]
+    mul_one := by simp [mul_one]
+    zero_mul := by simp [zero_mul]
+    pow_zero a := by simp
+    pow_succ a n := by simp [pow_succ]
+    ofNat_eq_natCast
+    | 0 => Nat.cast_zero.symm
+    | 1 => Nat.cast_one.symm
+    | n + 2 => rfl
+    ofNat_succ
+    | 0 => by simp [zero_add]
+    | 1 => by
+      change Nat.cast 2 = 1 + 1
+      rw [one_add_one_eq_two]
+      rfl
+    | n + 2 => by
+      change Nat.cast (n + 2 + 1) = Nat.cast (n + 2) + 1
+      rw [← AddMonoidWithOne.natCast_succ]
+    nsmul_eq_natCast_mul n a := nsmul_eq_mul n a }
 
 /-!
 demonstrating an error in grind
