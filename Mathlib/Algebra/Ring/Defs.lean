f99d273f15a853a628877d3493ce8ab9e2d46e31
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 13:16:40 +0100

    simplify

--- Mathlib/Algebra/Ring/Defs.lean
+++ Mathlib/Algebra/Ring/Defs.lean
@@ -22,20 +22,6 @@ open Function
 ### `Distrib` class
 -/
 
-/-!
-### Classes of semirings and rings
-
-We make sure that the canonical path from `NonAssocSemiring` to `Ring` passes through `Semiring`,
-as this is a path which is followed all the time in linear algebra where the defining semilinear map
-`σ : R →+* S` depends on the `NonAssocSemiring` structure of `R` and `S` while the module
-definition depends on the `Semiring` structure.
-
-It is not currently possible to adjust priorities by hand (see https://github.com/leanprover/lean4/issues/2115). Instead, the last
-declared instance is used, so we make sure that `Semiring` is declared after `NonAssocRing`, so
-that `Semiring -> NonAssocSemiring` is tried before `NonAssocRing -> NonAssocSemiring`.
-TODO: clean this once https://github.com/leanprover/lean4/issues/2115 is fixed
--/
-
 /-- An associative but not-necessarily unital semiring. -/
 class NonUnitalSemiring (α : Type u) extends SemigroupWithZero α
 
