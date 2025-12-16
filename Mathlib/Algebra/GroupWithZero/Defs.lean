ca2abe6e117298f1acbb560e18a916c730feda29
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 13:03:28 +0100

    simplify

--- Mathlib/Algebra/GroupWithZero/Defs.lean
+++ Mathlib/Algebra/GroupWithZero/Defs.lean
@@ -7,7 +7,7 @@ module
 
 public import Mathlib.Algebra.Group.Defs
 -- public import Mathlib.Logic.Nontrivial.Defs
-public import Mathlib.Logic.Basic
+-- public import Mathlib.Logic.Basic
 
 /-!
 # Typeclasses for groups with an adjoined zero element
