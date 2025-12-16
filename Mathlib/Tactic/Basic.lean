64a7151282f93b3329ece9211009c2ab8647bb2e
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 17:51:54 +0100

    simplify

--- Mathlib/Tactic/Basic.lean
+++ Mathlib/Tactic/Basic.lean
@@ -13,26 +13,3 @@ public meta import Mathlib.Tactic.TypeStar
 public meta import Mathlib.Tactic.Linter.OldObtain
 public meta import Mathlib.Tactic.Simproc.ExistsAndEq
 public import Batteries.Util.LibraryNote -- For `library_note` command.
-
-/-!
-# Basic tactics and utilities for tactic writing
-
-This file defines some basic utilities for tactic writing, and also
-- a dummy `variables` macro (which warns that the Lean 4 name is `variable`)
-- the `introv` tactic, which allows the user to automatically introduce the variables of a theorem
-and explicitly name the non-dependent hypotheses,
-- an `assumption` macro, calling the `assumption` tactic on all goals
-- the tactics `match_target` and `clear_aux_decl` (clearing all auxiliary declarations from the
-context).
--/
-
-public meta section
-
-namespace Mathlib.Tactic
-open Lean Parser.Tactic Elab Command Elab.Tactic Meta
-
-/-- Syntax for the `variables` command: this command is just a stub,
-and merely warns that it has been renamed to `variable` in Lean 4. -/
-syntax (name := «variables») "variables" (ppSpace bracketedBinder)* : command
-
-end Mathlib.Tactic
