75b98b16871842c148cb5bd1ffbd0f868cd11b4c
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 16:06:58 +0100

    simplify

--- Mathlib/Algebra/Group/Defs.lean
+++ Mathlib/Algebra/Group/Defs.lean
@@ -299,20 +299,6 @@ class MulOneClass (M : Type u) extends MulOne M where
   /-- One is a right neutral element for multiplication -/
   protected mul_one : ∀ a : M, a * 1 = a
 
-theorem MulOneClass.ext {M : Type u} : ∀ ⦃m₁ m₂ : MulOneClass M⦄, m₁.mul = m₂.mul → m₁ = m₂ := by
-  rintro @⟨@⟨⟨one₁⟩, ⟨mul₁⟩⟩, one_mul₁, mul_one₁⟩ @⟨@⟨⟨one₂⟩, ⟨mul₂⟩⟩, one_mul₂, mul_one₂⟩ ⟨rfl⟩
-  -- FIXME (See https://github.com/leanprover/lean4/issues/1711)
-  -- congr
-  suffices one₁ = one₂ by cases this; rfl
-  exact (one_mul₂ one₁).symm.trans (mul_one₁ one₂)
-
-section MulOneClass
-
-variable {M : Type u} [MulOneClass M]
-
-end MulOneClass
-
-
 /-!
 ### Design note on `AddMonoid` and `Monoid`
 
