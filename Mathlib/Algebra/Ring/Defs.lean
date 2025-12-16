c78c7cbcb6780099b35cce95157678002037dfa9
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 15:34:20 +0100

    simplify

--- Mathlib/Algebra/Ring/Defs.lean
+++ Mathlib/Algebra/Ring/Defs.lean
@@ -4,23 +4,3 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jeremy Avigad, Leonardo de Moura, Floris van Doorn, Yury Kudryashov, Neil Strickland
 -/
 module
-
-public import Mathlib.Algebra.GroupWithZero.Defs
-public import Mathlib.Data.Int.Cast.Defs
--- public import Mathlib.Tactic.Spread
--- public import Mathlib.Tactic.StacksAttribute
-
-@[expose] public section
-
-universe u v
-
-variable {α : Type u} {R : Type v}
-
-/-!
-### `Distrib` class
--/
-
-/-- A `Semiring` is a type with addition, multiplication, a `0` and a `1` where addition is
-commutative and associative, multiplication is associative and left and right distributive over
-addition, and `0` and `1` are additive and multiplicative identities. -/
-class Semiring (α : Type u) extends SemigroupWithZero α,  MulOneClass α, AddMonoidWithOne α
--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -5,7 +5,23 @@ Authors: Eric Rodriguez
 -/
 module
 
-public import Mathlib.Algebra.Ring.Defs
+public import Mathlib.Algebra.GroupWithZero.Defs
+public import Mathlib.Data.Int.Cast.Defs
+
+@[expose] public section
+
+universe u v
+
+variable {α : Type u} {R : Type v}
+
+/-!
+### `Distrib` class
+-/
+
+/-- A `Semiring` is a type with addition, multiplication, a `0` and a `1` where addition is
+commutative and associative, multiplication is associative and left and right distributive over
+addition, and `0` and `1` are additive and multiplicative identities. -/
+class Semiring (α : Type u) extends SemigroupWithZero α,  MulOneClass α, AddMonoidWithOne α
 -- public import Mathlib.Data.Int.Cast.Basic
 
 /-!
