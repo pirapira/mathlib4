ce02c8d085efe2094b567ac33fc1db4f9e8b25f8
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 15:41:40 +0100

    simplify

--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -7,8 +7,7 @@ module
 
 public import Mathlib.Algebra.GroupWithZero.Defs
 
-public import Mathlib.Algebra.Group.Defs
--- public import Mathlib.Data.Nat.Init
+-- public import Mathlib.Algebra.Group.Defs
 
 @[expose] public section
 
