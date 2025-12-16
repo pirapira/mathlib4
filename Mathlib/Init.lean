9e75055c553e5f0bb9cba7fcbd578157ae20b26f
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 18:08:04 +0100

    simplify

--- Mathlib/Init.lean
+++ Mathlib/Init.lean
@@ -2,9 +2,3 @@ module
 
 public import Lean.Linter.Sets -- for the definition of linter sets
 public import Lean.LibrarySuggestions.Default -- for `+suggestions` modes in tactics
-public import Mathlib.Tactic.Linter.CommandStart
-public import Mathlib.Tactic.Linter.DeprecatedSyntaxLinter
-public import Mathlib.Tactic.Linter.DirectoryDependency
-public import Mathlib.Tactic.Linter.DocPrime
-public import Mathlib.Tactic.Linter.DocString
-public import Mathlib.Tactic.Linter.EmptyLine
