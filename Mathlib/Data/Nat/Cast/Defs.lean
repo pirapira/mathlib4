954624fbee68ca0501b9185fc6bebce9bab2bbaa
Yoichi Hirai (Flamingo Ponderado Unipessoal LDA) <yoichi@flamingoponderado.com>
2025-12-16 15:35:37 +0100

    simplify

--- Mathlib/Data/Nat/Cast/Defs.lean
+++ Mathlib/Data/Nat/Cast/Defs.lean
@@ -4,26 +4,3 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Mario Carneiro, Gabriel Ebner
 -/
 module
-
-public import Mathlib.Algebra.Group.Defs
-public import Mathlib.Data.Nat.Init
-
-@[expose] public section
-
-variable {R : Type*}
-
-/-- Recognize numeric literals which are at least `2` as terms of `R` via `Nat.cast`. This
-instance is what makes things like `37 : R` type check.  Note that `0` and `1` are not needed
-because they are recognized as terms of `R` (at least when `R` is an `AddMonoidWithOne`) through
-`Zero` and `One`, respectively. -/
-@[nolint unusedArguments]
-instance (priority := 100) instOfNatAtLeastTwo {n : ℕ} [NatCast R] [Nat.AtLeastTwo n] :
-    OfNat R n where
-  ofNat := n.cast
-
-/-! ### Additive monoids with one -/
-
-/-- An `AddMonoidWithOne` is an `AddMonoid` with a `1`.
-It also contains data for the unique homomorphism `ℕ → R`. -/
-class AddMonoidWithOne (R : Type*) extends NatCast R, AddMonoid R, One R where
-  natCast := sorry
--- Mathlib/Data/ZMod/Defs.lean
+++ Mathlib/Data/ZMod/Defs.lean
@@ -6,7 +6,29 @@ Authors: Eric Rodriguez
 module
 
 public import Mathlib.Algebra.GroupWithZero.Defs
-public import Mathlib.Data.Nat.Cast.Defs
+
+public import Mathlib.Algebra.Group.Defs
+public import Mathlib.Data.Nat.Init
+
+@[expose] public section
+
+variable {R : Type*}
+
+/-- Recognize numeric literals which are at least `2` as terms of `R` via `Nat.cast`. This
+instance is what makes things like `37 : R` type check.  Note that `0` and `1` are not needed
+because they are recognized as terms of `R` (at least when `R` is an `AddMonoidWithOne`) through
+`Zero` and `One`, respectively. -/
+@[nolint unusedArguments]
+instance (priority := 100) instOfNatAtLeastTwo {n : ℕ} [NatCast R] [Nat.AtLeastTwo n] :
+    OfNat R n where
+  ofNat := n.cast
+
+/-! ### Additive monoids with one -/
+
+/-- An `AddMonoidWithOne` is an `AddMonoid` with a `1`.
+It also contains data for the unique homomorphism `ℕ → R`. -/
+class AddMonoidWithOne (R : Type*) extends NatCast R, AddMonoid R, One R where
+  natCast := sorry
 
 @[expose] public section
 
